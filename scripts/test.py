f061b48436a44da64324ed372a548569cd578905
Jon O'Neill <Jon.ONeill@iquw.com>
2025-08-17 13:32:49 +0100

    remove stuff from tests

--- scripts/test.py
+++
@@ -1,7 +0,0 @@
-import requests, json
-
-league_id = 12260
-choices = requests.get(f"https://draft.premierleague.com/api/draft/{league_id}/choices").json()
-
-with open("choices_dump.txt", "w", encoding="utf-8") as f:
-    json.dump(choices, f, indent=2)
